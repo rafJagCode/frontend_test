43bc9f2febc21b64aa7686c166d966f5913033e1
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-07-10 20:48:13 +0200

    Add company-name component

---
+++ recruitment-task/src/app/core/footer/components/company-name/company-name.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-name',
+  standalone: true,
+  imports: [],
+  templateUrl: './company-name.component.html',
+  styleUrl: './company-name.component.scss'
+})
+export class CompanyNameComponent {
+
+}

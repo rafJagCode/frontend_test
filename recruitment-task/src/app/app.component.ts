83f42a1a8e191944930df8ff2ec99f0c6a322ab1
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-07-10 16:45:49 +0200

    Create core layout

--- recruitment-task/src/app/app.component.ts
+++ recruitment-task/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { ContentComponent } from './core/content/content.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [HeaderComponent, ContentComponent, FooterComponent],
 })
 export class AppComponent {}

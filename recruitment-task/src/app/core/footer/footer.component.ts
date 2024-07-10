303d5258a5b981c4b73784832e3e0c6a31b9ad0c
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-07-10 21:53:19 +0200

    Add show-button component

--- recruitment-task/src/app/core/footer/footer.component.ts
+++ recruitment-task/src/app/core/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterIconComponent } from './components/footer-icon/footer-icon.component';
+import { CompanyNameComponent } from './components/company-name/company-name.component';
+import { ShowButtonComponent } from './components/show-button/show-button.component';
 
 @Component({
   selector: '[app-footer]',
   standalone: true,
-  imports: [FooterIconComponent],
+  imports: [FooterIconComponent, CompanyNameComponent, ShowButtonComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })

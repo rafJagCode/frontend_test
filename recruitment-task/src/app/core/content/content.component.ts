a5c3a1dbd7e28f4c6878e43ab7ec91e7fe035410
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-07-11 00:55:26 +0200

    Add first-block component

--- recruitment-task/src/app/core/content/content.component.ts
+++ recruitment-task/src/app/core/content/content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FirstBlockComponent } from '../../features/first-block/first-block.component';
 
 @Component({
   selector: '[app-content]',
   standalone: true,
-  imports: [],
+  imports: [FirstBlockComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss',
 })
--- recruitment-task/src/app/features/first-block/first-block.component.ts
+++ recruitment-task/src/app/features/first-block/first-block.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BlockTitleComponent } from '../../shared/block-title/block-title.component';
 
 @Component({
   selector: 'app-first-block',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, BlockTitleComponent],
   templateUrl: './first-block.component.html',
-  styleUrl: './first-block.component.scss'
+  styleUrl: './first-block.component.scss',
 })
 export class FirstBlockComponent {
-
+  title = 'block pierwszy';
+  options = [
+    { id: 1, label: 'Opcja pierwsza', value: 'first' },
+    { id: 2, label: 'Opcja druga', value: 'second' },
+    { id: 3, label: 'Opcja losowa', value: 'random' },
+  ];
+  selectedOption: any;
 }

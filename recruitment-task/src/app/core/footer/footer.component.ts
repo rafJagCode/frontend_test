3fffa0e07dec383019a31314284610b398fb53ef
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-07-10 20:36:50 +0200

    Create footer-icon component

--- recruitment-task/src/app/core/footer/footer.component.ts
+++ recruitment-task/src/app/core/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterIconComponent } from './components/footer-icon/footer-icon.component';
 
 @Component({
   selector: '[app-footer]',
   standalone: true,
-  imports: [],
+  imports: [FooterIconComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })

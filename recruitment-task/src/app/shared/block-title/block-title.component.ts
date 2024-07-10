46f525975c7e357c7b18a24e8477255122dd58c9
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-07-11 00:54:43 +0200

    Add block-title component

--- recruitment-task/src/app/shared/block-title/block-title.component.ts
+++ recruitment-task/src/app/shared/block-title/block-title.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-block-title',
   standalone: true,
   imports: [],
   templateUrl: './block-title.component.html',
-  styleUrl: './block-title.component.scss'
+  styleUrl: './block-title.component.scss',
 })
 export class BlockTitleComponent {
-
+  @Input() text?: string;
 }

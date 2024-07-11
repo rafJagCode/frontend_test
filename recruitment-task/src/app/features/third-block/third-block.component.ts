d7fd1a6199710e86ead3f35e343614d712e24025
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-07-11 22:00:10 +0200

    Inject text content service into third-block component

--- recruitment-task/src/app/features/third-block/third-block.component.ts
+++ recruitment-task/src/app/features/third-block/third-block.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { BlockTitleComponent } from '../../shared/block-title/block-title.component';
+import { TextContentService } from '../../services/text-content/text-content.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-third-block',
   standalone: true,
-  imports: [BlockTitleComponent],
+  imports: [AsyncPipe, BlockTitleComponent],
   templateUrl: './third-block.component.html',
   styleUrl: './third-block.component.scss',
 })
 export class ThirdBlockComponent {
-  title =
+  public title =
     'blok z długą nazwą która sama się przytnie gdy będzie dłuższa niż dostępne miejscę';
+
+  constructor(public textContentService: TextContentService) {}
 }

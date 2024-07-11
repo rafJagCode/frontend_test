15f6e54294dd6c180e4a15586c344f495045c28d
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-07-11 17:18:54 +0200

    Add second and third block to content

--- recruitment-task/src/app/core/content/content.component.ts
+++ recruitment-task/src/app/core/content/content.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FirstBlockComponent } from '../../features/first-block/first-block.component';
+import { SecondBlockComponent } from '../../features/second-block/second-block.component';
+import { ThirdBlockComponent } from '../../features/third-block/third-block.component';
 
 @Component({
   selector: '[app-content]',
   standalone: true,
-  imports: [FirstBlockComponent],
+  imports: [FirstBlockComponent, SecondBlockComponent, ThirdBlockComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss',
 })

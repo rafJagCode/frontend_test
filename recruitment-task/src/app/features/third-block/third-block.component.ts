46fffb83d02aba76ea1202521ed001c40461cf08
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-07-11 17:17:39 +0200

    Add thrid-block component

--- recruitment-task/src/app/features/third-block/third-block.component.ts
+++ recruitment-task/src/app/features/third-block/third-block.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { BlockTitleComponent } from '../../shared/block-title/block-title.component';
 
 @Component({
   selector: 'app-third-block',
   standalone: true,
-  imports: [],
+  imports: [BlockTitleComponent],
   templateUrl: './third-block.component.html',
-  styleUrl: './third-block.component.scss'
+  styleUrl: './third-block.component.scss',
 })
 export class ThirdBlockComponent {
-
+  title =
+    'blok z długą nazwą która sama się przytnie gdy będzie dłuższa niż dostępne miejscę';
 }

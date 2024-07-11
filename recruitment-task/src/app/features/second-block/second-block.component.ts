0451fcc4fc7409310401646a2d4e2c9d64942cfb
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-07-11 17:14:51 +0200

    Add second-block component

--- recruitment-task/src/app/features/second-block/second-block.component.ts
+++ recruitment-task/src/app/features/second-block/second-block.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { BlockTitleComponent } from '../../shared/block-title/block-title.component';
+import { ControlButtonComponent } from './components/control-button/control-button.component';
 
 @Component({
   selector: 'app-second-block',
   standalone: true,
-  imports: [],
+  imports: [BlockTitleComponent, ControlButtonComponent],
   templateUrl: './second-block.component.html',
-  styleUrl: './second-block.component.scss'
+  styleUrl: './second-block.component.scss',
 })
 export class SecondBlockComponent {
+  title = 'blok drugi';
 
+  replaceContent(): void {
+    console.log('replace');
+  }
+
+  appendContent(): void {
+    console.log('append');
+  }
 }

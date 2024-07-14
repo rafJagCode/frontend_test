7aaa2047646163f791fa3de50019b279487f6ef8
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-07-14 18:13:04 +0200

    Add modal to content

--- recruitment-task/src/app/core/content/content.component.ts
+++ recruitment-task/src/app/core/content/content.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { FirstBlockComponent } from '../../features/first-block/first-block.component';
 import { SecondBlockComponent } from '../../features/second-block/second-block.component';
 import { ThirdBlockComponent } from '../../features/third-block/third-block.component';
+import { LocalStorageCrudModalComponent } from '../local-storage-crud-modal/local-storage-crud-modal.component';
 
 @Component({
   selector: '[app-content]',
   standalone: true,
-  imports: [FirstBlockComponent, SecondBlockComponent, ThirdBlockComponent],
+  imports: [
+    FirstBlockComponent,
+    SecondBlockComponent,
+    ThirdBlockComponent,
+    LocalStorageCrudModalComponent,
+  ],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss',
   host: { class: 'content' },
